34e3a47cf30ef20316bf5d1bf84e731771ccda08
Lauler <fatonr@gmail.com>
2023-02-04 12:05:13 +0100

    preprocess progress prints

--- scripts/preprocess_speeches_metadata.py
+++ scripts/preprocess_speeches_metadata.py
@@ -2,6 +2,7 @@ import pandas as pd
 import argparse
 import json
 import os
+from tqdm import tqdm
 from src.data import preprocess_text
 
 parser = argparse.ArgumentParser(
@@ -12,22 +13,24 @@ parser.add_argument("-d", "--dest_folder", type=str, default="data")
 args = parser.parse_args()
 
 json_files = [
-    os.path.join(args.folder, filename)
-    for filename in os.listdir(args.folder)
-    if filename.endswith(".json")
+    os.path.join(args.folder, filename) for filename in os.listdir(args.folder) if filename.endswith(".json")
 ]
 
 json_speeches = []
-for file in json_files:
+
+for file in tqdm(json_files):
     with open(os.path.join(file), "r", encoding="utf-8-sig") as f:
         json_speeches.append(json.load(f)["anforande"])
 
+print("Normalizing json to dataframe...")
 df = pd.json_normalize(json_speeches)
 # df = df.drop(columns=["anforandetext"])
 df["anforande_nummer"] = df["anforande_nummer"].astype(int)
+print("Preprocessing text...")
 df = preprocess_text(df, textcol="anforandetext")
 
 df = df.sort_values(["dok_id", "anforande_nummer"]).reset_index(drop=True)
 df.loc[df["rel_dok_id"] == "", "rel_dok_id"] = None
 
+print(f"Saving file to {os.path.join(args.dest_folder, 'df_anforanden_metadata.parquet')}")
 df.to_parquet(os.path.join(args.dest_folder, "df_anforanden_metadata.parquet"))
